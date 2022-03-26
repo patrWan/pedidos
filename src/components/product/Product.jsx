ffdc4b2e224c031a80d8b73856d62be7710b3850
Patricio González Camilo <patricio.gonzalez.camilo@gmail.com>
2022-03-26 16:24:05 -0300

    Componente Product
    Se ajustaron la forma en que se muestran las imágenes

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
@@ -12,45 +12,41 @@ import ShoppingCartContext from '../../context/shoppingCart/ShoppingCartContext'
 import UserContext from '../../context/user/UserContext';
 
 const Product = (props) => {
-    
-    const {product} = props;
 
-    const {addProduct} = useContext(ShoppingCartContext);
+    const { product } = props;
+
+    const { addProduct } = useContext(ShoppingCartContext);
 
     const { userFrom } = useContext(UserContext);
 
-  return (
-    <div>
-        <Card sx={{ maxWidth: 345 , minHeight: 400, margin : 1}}>
-        <CardMedia
-            component="img"
-            height="100"
-            image={product.imageUrl}
-            alt="green iguana"
-            className="product-img"
-        />
-        <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-            {product.title}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-            {product.description}
-            </Typography>
-        </CardContent>
-        <CardActions>
-            <Button size="large" color="success" >${product.price}</Button>
-            
-            {userFrom ? 
-            <Button size="small" variant="contained" onClick={() => addProduct(product)}>Agregar</Button> 
-            : 
-            <Typography gutterBottom variant="body2" component="div" color="Highlight" fontSize={16}>
-                Inicie sesión para utilizar las funciones de usuario.
-            </Typography>
-            }
-        </CardActions>
-        </Card>
-    </div>
-  );
+    return (
+        <div>
+            <Card sx={{ maxWidth: 345, minHeight: 450, margin: 1 }}>
+                <CardMedia className='card-media'>
+                    <img src={product.imageUrl} className="product-img"/>
+                </CardMedia>
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        {product.title}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        {product.description}
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <Button size="large" color="success" >${product.price}</Button>
+
+                    {userFrom ?
+                        <Button size="small" variant="contained" onClick={() => addProduct(product)}>Agregar</Button>
+                        :
+                        <Typography gutterBottom variant="body2" component="div" color="Highlight" fontSize={16}>
+                            Inicie sesión para utilizar las funciones de usuario.
+                        </Typography>
+                    }
+                </CardActions>
+            </Card>
+        </div>
+    );
 }
 
 export default Product
